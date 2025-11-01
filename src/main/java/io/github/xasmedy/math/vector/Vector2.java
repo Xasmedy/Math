d97a73ceddb1e1fe8aca1036a4d860750b907398
xasmedy <xasmedy08cim@gmail.com>
2025-11-01 16:35:29 +0100

    Re-removed the float specialization in Vector2 It's a lot of noise while working on the implementation, and I hope that by shortening vector2 to v2, it's going to be way less annoying to work with.

--- src/main/java/io/github/xasmedy/math/vector/Vector2.java
+++ src/main/java/io/github/xasmedy/math/vector/Vector2.java
@@ -14,17 +14,9 @@ public value record Vector2(float x, float y) implements Vector<Vector2>, Point2
         return v2(x() + value.x(), y() + value.y());
     }
 
-    public Vector2 sum(float x, float y) {
-        return sum(v2(x, y));
-    }
-
     @Override
     public Vector2 sub(Vector2 value) {
         return v2(x() - value.x(), y() - value.y());
-    }
-
-    public Vector2 sub(float x, float y) {
-        return sub(v2(x, y));
     }
 
     @Override
@@ -93,65 +85,13 @@ public value record Vector2(float x, float y) implements Vector<Vector2>, Point2
         return this;
     }
 
-    public float dot(float x, float y) {
-        return dot(v2(x, y));
-    }
-
-    public float distance(float x, float y) {
-        return distance(v2(x, y));
-    }
-
-    public float distance2(float x, float y) {
-        return distance2(v2(x, y));
-    }
-
-    public Vector2 lerp(float x, float y, float alpha) {
-        return lerp(v2(x, y), alpha);
-    }
-
-    public Vector2 interpolate(float x, float y, float alpha, Function<Float, Float> interpolator) {
-        return interpolate(v2(x, y), alpha, interpolator);
-    }
-
     @Override
     public boolean isParallel(Vector2 vector, float epsilon) {
         return Math.abs(cross(vector)) <= epsilon;
     }
 
-    public boolean isParallel(float x, float y, float epsilon) {
-        return isParallel(v2(x, y), epsilon);
-    }
-
-    public boolean isCollinear(float x, float y, float epsilon) {
-        return isCollinear(v2(x, y), epsilon);
-    }
-
-    public boolean isCollinearOpposite(float x, float y, float epsilon) {
-        return isCollinearOpposite(v2(x, y), epsilon);
-    }
-
-    public boolean isPerpendicular(float x, float y, float epsilon) {
-        return isPerpendicular(v2(x, y), epsilon);
-    }
-
-    public boolean hasSameDirection(float x, float y) {
-        return hasSameDirection(v2(x, y));
-    }
-
-    public boolean hasOppositeDirection(float x, float y) {
-        return hasOppositeDirection(v2(x, y));
-    }
-
-    public boolean epsilonEquals(float x, float y, float epsilon) {
-        return epsilonEquals(v2(x, y), epsilon);
-    }
-
     public float cross(Vector2 vector) {
         return x() * vector.y() - y() * vector.x();
-    }
-
-    public float cross(float x, float y) {
-        return cross(v2(x, y));
     }
 
     public Vector3 withZ(float z) {
