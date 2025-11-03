343e630a73544a5190ac3b59ed1e1ccd9f1f4a16
xasmedy <xasmedy08cim@gmail.com>
2025-11-03 20:21:14 +0100

    Generalized `Rectangle` and provided different implementations

--- src/main/java/io/github/xasmedy/math/shape/Rectangle.java
+++ src/main/java/io/github/xasmedy/math/shape/Rectangle.java
@@ -2,6 +2,33 @@ package io.github.xasmedy.math.shape;
 
 import io.github.xasmedy.math.point.Point2;
 import jdk.internal.vm.annotation.LooselyConsistentValue;
+import jdk.internal.vm.annotation.NullRestricted;
 
-@LooselyConsistentValue
-public value record Rectangle(Point2<Float> position, float width, float height) {}
+public interface Rectangle<T extends Number, P extends Point2<T>> {
+
+    P position();
+
+    T width();
+
+    T height();
+
+    @LooselyConsistentValue
+    value record I32(@NullRestricted Point2.I32 position,
+                     @NullRestricted Integer width,
+                     @NullRestricted Integer height      ) implements Rectangle<Integer, Point2.I32> {}
+
+    @LooselyConsistentValue
+    value record I64(@NullRestricted Point2.I64 position,
+                     @NullRestricted Long width,
+                     @NullRestricted Long height         ) implements Rectangle<Long, Point2.I64> {}
+
+    @LooselyConsistentValue
+    value record F32(@NullRestricted Point2.F32 position,
+                     @NullRestricted Float width ,
+                     @NullRestricted Float height        ) implements Rectangle<Float, Point2.F32> {}
+
+    @LooselyConsistentValue
+    value record F64(@NullRestricted Point2.F64 position,
+                     @NullRestricted Double width,
+                     @NullRestricted Double height       ) implements Rectangle<Double, Point2.F64> {}
+}
