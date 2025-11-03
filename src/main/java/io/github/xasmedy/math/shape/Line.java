e63094b895f2260dbfbbd43d7d22506041ea020b
xasmedy <xasmedy08cim@gmail.com>
2025-11-03 20:17:15 +0100

    Generalized `Line` and provided different implementations

--- src/main/java/io/github/xasmedy/math/shape/Line.java
+++ src/main/java/io/github/xasmedy/math/shape/Line.java
@@ -2,6 +2,27 @@ package io.github.xasmedy.math.shape;
 
 import io.github.xasmedy.math.point.Point2;
 import jdk.internal.vm.annotation.LooselyConsistentValue;
+import jdk.internal.vm.annotation.NullRestricted;
 
-@LooselyConsistentValue
-public value record Line(Point2<Float> start, Point2<Float> end) {}
+public interface Line<P extends Point2<?>> {
+
+    P start();
+
+    P end();
+
+    @LooselyConsistentValue
+    value record I32(@NullRestricted Point2.I32 start,
+                     @NullRestricted Point2.I32 end   ) implements Line<Point2.I32> {}
+
+    @LooselyConsistentValue
+    value record I64(@NullRestricted Point2.I64 start,
+                     @NullRestricted Point2.I64 end   ) implements Line<Point2.I64> {}
+
+    @LooselyConsistentValue
+    value record F32(@NullRestricted Point2.F32 start,
+                     @NullRestricted Point2.F32 end   ) implements Line<Point2.F32> {}
+
+    @LooselyConsistentValue
+    value record F64(@NullRestricted Point2.F64 start,
+                     @NullRestricted Point2.F64 end   ) implements Line<Point2.F64> {}
+}
